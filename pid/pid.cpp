4a5714001e49b684322d065655193d25bd0e3392
Dragos Tanasa <dragos.tanasa99@gmail.com>
2023-11-07 16:12:55 +0100

    added antiwindup on PID

--- pid/pid.cpp
+++ pid/pid.cpp
@@ -5,21 +5,55 @@
     
     TODO: 
 
-        1. Implement anti-windup on integral term
+        1. Implement anti-windup on integral term -- FATTOOO piu o meno
         2. Implement derivative on measurement
+        3. Implemete low pass filter on derivative term  --> IL TAU sarà due volte la frequenza di campionamento
 */
 
 class PIDController {
 public:
-    PIDController(double kp, double ki, double kd) : kp_(kp), ki_(ki), kd_(kd), integral_(0), previous_error_(0) {}
+    PIDController(double kp, double ki, double kd) : kp_(kp), ki_(ki), kd_(kd), integral_(0), previous_error_(0), antiwindup_(false) {}
 
     double Calculate(double setpoint, double current_value) {
-        double error = setpoint - current_value;
-        integral_ += error;
-        double derivative = error - previous_error_;
-        double output = kp_ * error + ki_ * integral_ + kd_ * derivative;
+        double error = setpoint - current_value; // vel desiderata - vel attuale
+        double output = 0.0;
+        double derivative = (error - previous_error_);
+        // derivative = lastDerivative + (timeDelta / (timeDelta + RC)) * (derivative - lastDerivative);
+
+        if (antiwindup_ == false) {
+            std::cerr <<"antiwindup false"<<std::endl;
+            integral_ += error;
+            double output = kp_*error + ki_ * integral_ + kd_ * derivative;
+        } 
+        else {
+            std::cerr <<"antiwindup true"<<std::endl;
+            double output = kp_ * error + kd_ * derivative;
+            antiwindup_ = false;
+        }
+        
+        double clamped_output = clamping(output);
+        bool antiwindup_ = antiwindup(output, clamped_output, error);
         previous_error_ = error;
+
+        return clamped_output;
+    }
+
+    double clamping(double output) {
+        if (output > 100) {
+            output = 100;
+        }
+        else if (output < -100) {
+            output = -100;
+        }
         return output;
+    }
+
+    bool antiwindup(double output, double clamped_output, double error) {
+
+        if ((clamped_output != output) && (error * output > 0)) {
+            return true;
+        }
+        return false;
     }
 
 private:
@@ -28,6 +62,8 @@ private:
     double kd_;
     double integral_;
     double previous_error_;
+    // double lastDerivative_;
+    bool antiwindup_;
 };
 
 int main() {
